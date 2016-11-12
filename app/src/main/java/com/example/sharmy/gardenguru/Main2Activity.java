29b41bacc1eccabb2ab3bea631e84ac0f67aa5de
luckysharms9 <streethoops23@hotmail.com>
2016-11-12 13:10:57 -0700

    4th Commit
    MyGarden button works on homescreen

---
+++ app/src/main/java/com/example/sharmy/gardenguru/Main2Activity.java
@@ -0,0 +1,14 @@
+package com.example.sharmy.gardenguru;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Get the view from new_activity.xml
+        setContentView(R.layout.main2activity);
+    }
+}
--- app/src/main/java/com/example/sharmy/gardenguru/MainActivity.java
+++ app/src/main/java/com/example/sharmy/gardenguru/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.sharmy.gardenguru;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    Button button;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Get the view from activity_main.xml
         setContentView(R.layout.activity_main);
+
+        // Locate the button in activity_main.xml
+        button = (Button) findViewById(R.id.mygarden);
+
+        // Capture button clicks
+        button.setOnClickListener(new OnClickListener() {
+            public void onClick(View arg0) {
+
+                // Start NewActivity.class
+                Intent myIntent = new Intent(MainActivity.this,
+                        Main2Activity.class);
+                startActivity(myIntent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.layout.activity_main, menu);
+        return true;
     }
 }
