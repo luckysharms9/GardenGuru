b8ce85cbd5f9ef352ca644516b5aa92790fc6419
Rachel Solski <rachel.solski@gmail.com>
2016-11-12 18:45:33 -0700

    added AddGarden design

--- app/src/main/java/com/example/sharmy/gardenguru/AddGarden.java
+++ app/src/main/java/com/example/sharmy/gardenguru/AddGarden.java
@@ -1,8 +1,10 @@
 package com.example.sharmy.gardenguru;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 
 public class AddGarden extends AppCompatActivity {
@@ -13,6 +15,10 @@ public class AddGarden extends AppCompatActivity {
         setContentView(R.layout.activity_add_garden);
     }
 
-
+    public void buttonSavedGarden(View v){
+        Button button = (Button) v;
+        Intent myIntent = new Intent(AddGarden.this, SavedGarden.class);
+        startActivity(myIntent);
+    }
 
 }
