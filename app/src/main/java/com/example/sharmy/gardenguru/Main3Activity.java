55f45d3f48bf1aba59d3c9883b584ef22b3e9516
luckysharms9 <streethoops23@hotmail.com>
2016-11-13 19:59:13 -0700

    Plants view edited, changes made. Plants view still not working correctly.

--- app/src/main/java/com/example/sharmy/gardenguru/Main3Activity.java
+++ app/src/main/java/com/example/sharmy/gardenguru/Main3Activity.java
@@ -2,12 +2,63 @@ package com.example.sharmy.gardenguru;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.HeaderViewListAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class Main3Activity extends AppCompatActivity {
+    //LIST OF ARRAY STRINGS WHICH WILL SERVE AS LIST ITEMS
+    ArrayList<String> listItems = new ArrayList<String>();
+
+    //DEFINING A STRING ADAPTER WHICH WILL HANDLE THE DATA OF THE LISTVIEW
+    ArrayAdapter<String> adapter;
+
+    //RECORDING HOW MANY TIMES THE BUTTON HAS BEEN CLICKED
+    int clickCounter = 0;
+    private ListView mListView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
         setContentView(R.layout.activity_main3);
+
+        if (mListView == null) {
+            mListView = (ListView) findViewById(R.id.listDemo);
+        }
+
+        adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                listItems);
+        setListAdapter(adapter);
+    }
+
+    //METHOD WHICH WILL HANDLE DYNAMIC INSERTION
+    public void addItems(View v) {
+        listItems.add("Clicked : " + clickCounter++);
+        adapter.notifyDataSetChanged();
+    }
+
+    protected ListView getListView() {
+        if (mListView == null) {
+            mListView = (ListView) findViewById(R.id.listDemo);
+        }
+        return mListView;
+    }
+
+    protected void setListAdapter(ListAdapter adapter) {
+        getListView().setAdapter(adapter);
+    }
+
+    protected ListAdapter getListAdapter() {
+        ListAdapter adapter = getListView().getAdapter();
+        if (adapter instanceof HeaderViewListAdapter) {
+            return ((HeaderViewListAdapter) adapter).getWrappedAdapter();
+        } else {
+            return adapter;
+        }
     }
 }
--- app/src/main/java/com/example/sharmy/gardenguru/MainActivity.java
+++ app/src/main/java/com/example/sharmy/gardenguru/MainActivity.java
@@ -41,6 +41,7 @@ public class MainActivity extends Activity {
                 startActivity(myIntent);
             }
         });
+
     }
 
     @Override
