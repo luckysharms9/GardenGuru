f4e7b32cd4556694d165d94acaabd3b1a80b03b6
Rachel <rachel.solski@gmail.com>
2016-11-24 17:55:02 -0700

    List in pop up notification

--- app/src/main/java/com/example/sharmy/gardenguru/Confirm.java
+++ app/src/main/java/com/example/sharmy/gardenguru/Confirm.java
@@ -3,8 +3,14 @@ package com.example.sharmy.gardenguru;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 
 public class Confirm extends AppCompatActivity {
+
+    // Array of strings
+    String[] gardenArray = {"Tomato", "Patio", "Stuff"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +24,11 @@ public class Confirm extends AppCompatActivity {
         int height = myDisplay.heightPixels;
 
         getWindow().setLayout( (int) (width *.8 ), (int) (height *.55));
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_listview, gardenArray);
+
+        ListView listView = (ListView) findViewById(R.id.garden_list);
+        listView.setAdapter(adapter);
+
     }
 }
