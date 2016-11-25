d2af388f38c946b50eed8933ebfaf3be64f17fc8
Rachel <rachel.solski@gmail.com>
2016-11-24 20:38:59 -0700

    Connected tomato garden in list view to new class

--- app/src/main/java/com/example/sharmy/gardenguru/AddGarden.java
+++ app/src/main/java/com/example/sharmy/gardenguru/AddGarden.java
@@ -21,4 +21,8 @@ public class AddGarden extends AppCompatActivity {
         startActivity(myIntent);
     }
 
+
+
+
+
 }
--- app/src/main/java/com/example/sharmy/gardenguru/Confirm.java
+++ app/src/main/java/com/example/sharmy/gardenguru/Confirm.java
@@ -1,8 +1,10 @@
 package com.example.sharmy.gardenguru;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -29,11 +31,9 @@ public class Confirm extends AppCompatActivity {
         int width = myDisplay.widthPixels;
         int height = myDisplay.heightPixels;
 
-        getWindow().setLayout( (int) (width *.8 ), (int) (height *.55));
+        getWindow().setLayout((int) (width * .8), (int) (height * .55));
 
         ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_listview, gardenArray);
-
-
 
 
         ListView listView = (ListView) findViewById(R.id.garden_list);
@@ -41,16 +41,28 @@ public class Confirm extends AppCompatActivity {
 
         layoutinflater = getLayoutInflater();
 
-        ViewGroup header = (ViewGroup)layoutinflater.inflate(R.layout.header,listView,false);
+        ViewGroup header = (ViewGroup) layoutinflater.inflate(R.layout.header, listView, false);
 
         listView.addHeaderView(header);
 
         listView.setAdapter(adapter);
 
-
-
-
-
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arrayAdapter, View view, int position, long arg) {
+                if (position == 1) {
+                    Intent appInfo = new Intent(Confirm.this, GardenSpecs.class);
+                    startActivity(appInfo);
+                }
+                /**if(position==1){
+                 Intent english=new Intent(SwahiliService.this,EnglishService.class);
+                 startActivity(english);
+                 }
+                 if(position==2){
+                 Toast.makeText(getApplicationContext(),"You have selected pst3", Toast.LENGTH_LONG).show();
+                 }**/
+            }
+        });
 
 
     }
