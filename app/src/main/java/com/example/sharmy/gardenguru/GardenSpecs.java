4c2cf84defb498a541e011fd64555d25b3cca340
luckysharms9 <streethoops23@hotmail.com>
2016-11-26 12:26:21 -0700

    fixed plants page

--- app/src/main/java/com/example/sharmy/gardenguru/GardenSpecs.java
+++ app/src/main/java/com/example/sharmy/gardenguru/GardenSpecs.java
@@ -1,13 +1,18 @@
 package com.example.sharmy.gardenguru;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class GardenSpecs extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Get the view from activity_main.xml
         setContentView(R.layout.activity_garden_specs);
     }
 }
